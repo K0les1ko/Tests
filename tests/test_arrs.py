473eb2bad34e6002d03f680dc65b89ad42d74cfc
K0les1ko <mujljluoh@mail.ru>
2023-10-09 23:17:08 +0300

    fix_

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,12 +1,26 @@
-import pytest
-
-
 from Tests.utils.arrs import get, my_slice
+
 
 def test_get():
     assert get([1, 2, 3], 1) == 2
     assert get([1, 2, 3], 5, default="Not Found") == "Not Found"
 
+
 def test_my_slice():
     assert my_slice([1, 2, 3, 4, 5], start=1, end=4) == [2, 3, 4]
     assert my_slice([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+
+def test_my_slice_negative_indices():
+    array = [1, 2, 3, 4, 5]
+    assert my_slice(array, start=-3, end=-1) == [3, 4]
+
+def test_my_slice_start():
+    array = [1, 2, 3, 4, 5]
+    assert my_slice(array, start=3, end=2) == []
+
+def test_my_slice_empty():
+    assert my_slice([], start=1, end=3) == []
+
+def test_my_slice_indices():
+    array = [1, 2, 3]
+    assert my_slice(array, start=0, end=10) == [1, 2, 3]
